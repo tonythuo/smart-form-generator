410a34384be1d7404e88ac5b68f583c537aac8ef
Tony Thuo <tonythuo2005@gmail.com>
2025-12-03 15:34:53 +0200

    Added ability for ai agent to generate a form from a prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,40 @@
-import Image from "next/image";
+import { generateObject } from "ai"
+import { z } from "zod"
+import { google } from "@ai-sdk/google";
 
-export default function Home() {
-  return (
-  );
-}
+export default async function Home() {
+
+  const model = google("gemini-2.5-flash")
+
+  const generateForm = async (
+    prompt: string,
+  ) => {
+    const { object } = await generateObject({
+      model,
+      schema: z.array(z.object({
+        field: z.string(),
+        type: z.string(),
+        label: z.string(),
+      })),
+      prompt,
+    });
+
+    return object
+  };
+
+  const form = await generateForm("Please create form for signing up for event")
+
+  console.log(form)
+
+  return (<form>
+    {
+      form.map(({ field, type, label }) => (
+        <div key={field}>
+          <label htmlFor={field} >{label}</label>
+          <input type={type} id={field} name={label} required />
+        </div>
+      ))
+    }
+  </form>)
+  }
+
