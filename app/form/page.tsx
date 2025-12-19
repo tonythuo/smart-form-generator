ece167285e98525f78794d362bc24841e8a09881
Tony Thuo <tonythuo2005@gmail.com>
2025-12-19 14:07:16 +0200

    Added ability for user to query chatbot to generate a form

---
+++ app/form/page.tsx
@@ -0,0 +1,56 @@
+import { generateObject } from "ai"
+import { z } from "zod"
+import { google } from "@ai-sdk/google";
+import Form from "next/form";
+
+const model = google("gemini-2.5-flash")
+
+const generateForm = async (
+  prompt: string,
+) => {
+  const { object } = await generateObject({
+    model,
+    schema: z.array(z.object({
+      field: z.string(),
+      type: z.string(),
+      label: z.string(),
+    })),
+    prompt,
+  });
+
+  return object
+};
+
+async function processForm(formData: FormData) {
+  'use server'
+
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  }
+}
+
+export default async function Page({
+  searchParams
+}: {
+  searchParams: Promise<{ [key: string]: string }>
+}) {
+
+  let query = (await searchParams).query
+
+  const form = await generateForm(query)
+
+  return (
+    <Form action={processForm}>
+      {
+        form.map(({ field, type, label }) => (
+          <div key={field}>
+            <label htmlFor={field} >{label}</label>
+            <input type={type} id={field} name={label} required />
+          </div>
+        ))
+      }
+    </Form>)
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,14 @@
-import { generateObject } from "ai"
-import { z } from "zod"
-import { google } from "@ai-sdk/google";
+import Form from "next/form";
 
-export default async function Home() {
+export default async function Page() {
+  return (
+    <Form action="/form">
+      <input name="query" />
+      <button type="submit">Generate form</button>
+    </Form>
+  )
+}
 
-  const model = google("gemini-2.5-flash")
 
-  const generateForm = async (
-    prompt: string,
-  ) => {
-    const { object } = await generateObject({
-      model,
-      schema: z.array(z.object({
-        field: z.string(),
-        type: z.string(),
-        label: z.string(),
-      })),
-      prompt,
-    });
 
-    return object
-  };
-
-  const form = await generateForm("Please create form for signing up for event")
-
-  console.log(form)
-
-  return (<form>
-    {
-      form.map(({ field, type, label }) => (
-        <div key={field}>
-          <label htmlFor={field} >{label}</label>
-          <input type={type} id={field} name={label} required />
-        </div>
-      ))
-    }
-  </form>)
-  }
 
